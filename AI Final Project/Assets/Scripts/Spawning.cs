30f7d7fb02a2f6696add335545c936a2615766bb
Amtoj <37938445+Amtoj@users.noreply.github.com>
2020-11-16 14:25:32 -0500

    Removed second set of spawn code in the script and added a second hazard manager
    Some hazards might still spawn on top of each other though

--- AI Final Project/Assets/Scripts/Spawning.cs
+++ AI Final Project/Assets/Scripts/Spawning.cs
@@ -12,12 +12,6 @@ public class Spawning : MonoBehaviour
     // How much time between each spawn
     float timeGap;
 
-    // How much time between each offset spawn
-    float timeGapOff;
-
-    // Set the offset between the two spawners
-    public int offset;
-
     // Used to radomly choose which hazard will spawn
     int rand;
 
@@ -30,7 +24,6 @@ public class Spawning : MonoBehaviour
     {
         // Makes it easier to make use of the original time set manually by copying it over to another value
         timeGap = startTimer;
-        timeGapOff = startTimer * offset;
     }
 
     // Update is called once per frame
@@ -61,7 +54,7 @@ public class Spawning : MonoBehaviour
 
                 // Spawn the bird low if 0
                 if (rand == 0)
-                    Instantiate(bird, new Vector3(9.5f, 2.5f, 0), Quaternion.identity);
+                    Instantiate(bird, new Vector3(9.5f, 2.0f, 0), Quaternion.identity);
 
                 // Spawn the bird high if 1
                 if (rand == 1)
@@ -69,41 +62,6 @@ public class Spawning : MonoBehaviour
             }
             // Reset the timer to the original value
             timeGap = startTimer;
-        }
-
-        // Will keep counting down if still above zero
-        if (timeGapOff > 0)
-        {
-            timeGapOff -= Time.deltaTime;
-        }
-
-        // Will reset the timer and spawn a hazard if under zero
-        else if (timeGapOff <= 0)
-        {
-            // Randomizes a 0 or a 1 value
-            rand = Random.Range(0, 6);
-
-            if (rand == 3 || rand == 4)
-            {
-                Instantiate(cactus, new Vector3(9.5f, 0.9f, 0), Quaternion.identity);
-            }
-
-            // Spawn a bird if 1
-            if (rand == 5)
-            {
-                // Randomize a 0 or 1 value
-                rand = Random.Range(0, 2);
-
-                // Spawn the bird low if 0
-                if (rand == 0)
-                    Instantiate(bird, new Vector3(9.5f, 2.5f, 0), Quaternion.identity);
-
-                // Spawn the bird high if 1
-                if (rand == 1)
-                    Instantiate(bird, new Vector3(9.5f, 3.5f, 0), Quaternion.identity);
-            }
-
-            timeGapOff = startTimer / offset;
         }
     }
 }
